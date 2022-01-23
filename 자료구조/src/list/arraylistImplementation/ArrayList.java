8ff8f9e4e8b0a3bea60217f6833412645cba0e05
효정이 <w2029@e-mirim.hs.kr>
2022-01-23 23:13:22 +0900

    finish~

---
+++ 자료구조/src/list/arraylistImplementation/ArrayList.java
@@ -0,0 +1,48 @@
+package list.arraylistImplementation;
+
+public class ArrayList {
+	private int size = 0;
+	private Object[] elementData = new Object[100];
+	
+	public boolean addFirst(Object element) {
+		return add(0, element);
+	}
+	public boolean addLast(Object element) {
+		elementData[size] = element;
+		size++;
+		return true;
+	}
+	public boolean add(int index, Object element) {
+		for(int i = size - 1; i >= index; i--) {
+			elementData[i+1] = elementData[i];
+		}
+		elementData[index] = element;
+		size++;
+		return true;
+	}
+	public Object get(int index) {
+		return elementData[index];
+	}
+	public String toString() {
+		String str = "[";
+		for(int i=0; i < size; i++) {
+			str += elementData[i];
+			if(i < size-1) {
+				str += ",";
+			}
+		}
+		return str + "]";
+	}
+	public int size() {
+		return size;
+	}
+	public int indexOf(Object o) {
+		for(int i=0; i<size; i++) {
+			if(o.equals(elementData[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+}
---
+++ 자료구조/src/list/arraylistImplementation/Main.java
@@ -0,0 +1,30 @@
+package list.arraylistImplementation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList numbers = new ArrayList();
+		numbers.addLast(10);
+		numbers.addLast(20);
+		numbers.addLast(30);
+		numbers.addLast(40);
+		
+//		System.out.println(numbers.get(0));
+//		System.out.println(numbers.get(1));
+//		System.out.println(numbers.get(2));
+//		System.out.println(numbers.get(3));
+		
+//		numbers.add(1, 15);
+//		numbers.addFirst(5);
+//		
+//		System.out.println(numbers);
+		
+//		System.out.println(numbers.size());
+		
+		System.out.println(numbers.indexOf(70));
+
+	}
+
+}
